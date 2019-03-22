bca59e23efd3219e1d259e1887b5dcf2ba40f69f
alexander-hagg <alexander.hagg@h-brs.de>
2019-03-22 17:39:40 +0100

    Update

---
+++ blobSlider.m
@@ -0,0 +1,49 @@
+zahlBlobs = 1;
+[~,blobGenen,wieGross,gridBlobs] = randomBlob(zahlBlobs,elefant);
+blob = phenotypBlob(blobGenen,wieGross,gridBlobs);
+
+blob_slider(blobGenen,wieGross,gridBlobs);
+
+function blob_slider(blobGenen,wieGross,gridBlobs)
+hfig = figure(3);
+blob = phenotypBlob(blobGenen,wieGross,gridBlobs);
+imagesc(blob); colormap([0 0 0; 1 1 1]); title('Wie bewegt sich der Blob?');
+setappdata(hfig,'genom',blobGenen);
+slider1 = uicontrol('Parent', hfig,'Style','slider',...
+    'Units','normalized',...
+    'Position',[0.3 0.0 0.4 0.05],...
+    'Tag','slider1',...
+    'UserData',struct('val',blobGenen(2),'blobGenen',blobGenen,'wieGross',wieGross,'gridBlobs',gridBlobs),...
+    'Callback',@slider_callback);
+
+slider2 = uicontrol('Parent', hfig,'Style','slider',...
+    'Units','normalized',...
+    'Position',[0.3 0.05 0.4 0.05],...
+    'Tag','slider2',...
+    'UserData',struct('val',blobGenen(3),'blobGenen',blobGenen,'wieGross',wieGross,'gridBlobs',gridBlobs),...
+    'Callback',@slider_callback_2);
+
+
+slider1.Value = blobGenen(2);
+slider2.Value = blobGenen(3);
+end
+
+function slider_callback(hObject,eventdata)
+h = findobj('Tag','slider1');
+data = h.UserData;
+genom = getappdata(hObject.Parent,'genom');
+genom(2) = hObject.Value;
+blob = phenotypBlob(genom,data.wieGross,data.gridBlobs);
+setappdata(hObject.Parent,'genom',genom);
+imagesc(blob);
+end
+
+function slider_callback_2(hObject,eventdata)
+h = findobj('Tag','slider2');
+data = h.UserData;
+genom = getappdata(hObject.Parent,'genom');
+genom(3) = hObject.Value;
+blob = phenotypBlob(genom,data.wieGross,data.gridBlobs);
+setappdata(hObject.Parent,'genom',genom);
+imagesc(blob)
+end
--- demo.m
+++ demo.m
@@ -1,41 +1,21 @@
-
 %% % Zeige den Elefanten
 elefant = logical(imread('elefant-50x50.png'));
-%elefant = logical(imread('elefant-2-100x100.png'));
-%elefant = ~logical(imread('myelephant.bmp'));
-%elefant = logical(imread('mycircle.png'));
+figure(1); imagesc(elefant); colormap([0 0 0; 1 1 1]); title('Wie entsteht aus einem Blob einen Elefanten?'); grid on; grid minor;
 
-figure(1); imagesc(elefant); colormap([0 0 0; 1 1 1]); title('Elefant'); grid on; grid minor;
-                                                                            
+
+%% % Zeige den Kreis
+elefant = logical(imread('mycircle.png'));
+figure(1); imagesc(elefant); colormap([0 0 0; 1 1 1]); title('Versuchen wir zuerst einen runden Blob zu bilden.'); grid on; grid minor;
 
 %% % Zeige einen Blob
 zahlBlobs = 1;
 [~,blobGenen,wieGross,gridBlobs] = randomBlob(zahlBlobs,elefant);
 blob = phenotypBlob(blobGenen,wieGross,gridBlobs);
+figure(2); imagesc(blob); colormap([0 0 0; 1 1 1]); title('Das ist ein willkürlicher Blob.'); grid on; grid minor;
+          
 
-genEins = 0.5;
-genZwei = 0.5;
-genDrei = 0.5;
-
-f = figure(2); colormap([0 0 0; 1 1 1]);
-ax = axes('Parent',f,'position',[0.13 0.39  0.77 0.7]);
-h = imagesc(ax,blob);
-
-b = uicontrol('Parent',f,'Style','slider','Position',[81,54,419,23],...
-              'value',genEins, 'min',0, 'max',1);
-bgcolor = f.Color;
-bl1 = uicontrol('Parent',f,'Style','text','Position',[50,54,23,23],...
-                'String','0','BackgroundColor',bgcolor);
-bl2 = uicontrol('Parent',f,'Style','text','Position',[500,54,23,23],...
-                'String','1','BackgroundColor',bgcolor);
-bl3 = uicontrol('Parent',f,'Style','text','Position',[240,25,100,23],...
-                'String','Damping Ratio','BackgroundColor',bgcolor);
-
-b.Callback = @(blobGenen) imagesc(h, phenotypBlob(blobGenen,wieGross,gridBlobs) ); 
-% Ist der Blob ein Elefant? Frag dem Orakel. Es weiß alles.
-%[qualitaet] = orakel(blob,elefant);
-
-%figure(2); zeigeblob(blob); grid on; grid minor; title(['Blob Qualität: ' num2str(qualitaet) ' /100']);
+%% % Ändere den Blob
+blobSlider;
 
 %% % Evolviere den Blob zum Elefanten
 zahlBlobs = 100;
